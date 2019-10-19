060d9a9bcfb7b5386bfd8733cb77bd449e6e02cc
Ahmed__Ehab <ahmedehab11@gmail.com>
2019-10-19 13:48:20 +0200

    Sort has been added

---
+++ Sort.h
@@ -0,0 +1,17 @@
+
+
+#ifndef _Sort_h
+
+#define _Sort_h
+
+
+//------------------- Prototype of Functions ---------------//
+
+
+extern void bubble_sort (int *,int );
+extern void selection_sort(int *, int );
+
+extern void merge_partition (int * , int , int , int );
+extern void merge_sort (int * , int , int ,int ,int ,int );
+
+#endif
